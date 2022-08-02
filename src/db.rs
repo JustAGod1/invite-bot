c0b20f7a1dfc16e141a425c2a28623e09365dd7b
JustAGod <hvalick2111@gmail.com>
2022-08-02 04:45:33 +0300

    initial

---
+++ src/db.rs
@@ -0,0 +1,118 @@
+use std::sync::{Arc, Mutex};
+use sqlite::{Connection, Cursor, OpenFlags, Row, State, Value};
+
+pub struct User {
+    pub full_name: String,
+    pub telegram_id: Option<String>,
+}
+
+pub struct DBConn {
+   conn: Mutex<Connection>
+}
+
+unsafe impl Send for DBConn {}
+unsafe impl Sync for DBConn {}
+
+
+impl DBConn {
+
+    pub fn open() -> Result<DBConn, String> {
+        sqlite::open("db.sqlite")
+            .map(|a| DBConn {
+            conn: Mutex::new(a)
+        })
+        .map_err(|a| a.to_string())
+    }
+
+    fn build_from_cursor(cursor: &Row) -> Result<User, String> {
+        Ok(User {
+            full_name: cursor.try_get::<String, _>(0).map_err(|a| a.to_string())?,
+            telegram_id: cursor.try_get::<Option<String>, _>(1).map_err(|a| a.to_string())?,
+        })
+    }
+
+    pub fn find_by_full_name(&self, name: &str) -> Result<Option<User>, String> {
+        let conn = self.conn.lock()
+            .map_err(|a| a.to_string())?;
+        let mut cursor = conn
+            .prepare("SELECT full_name, telegram_id FROM users WHERE full_name = ? LIMIT 1")
+            .map_err(|a| a.to_string())?
+            .into_cursor()
+            .bind(&[Value::String(name.to_string())])
+            .map_err(|a| a.to_string())?;
+
+        if let Some(Ok(row)) = cursor.next() {
+            let user = Self::build_from_cursor(&row)?;
+
+            return Ok(Some(user));
+        }
+
+        Ok(None)
+    }
+
+    pub fn find_by_telegram_id(&self, id: u64) -> Result<Option<User>, String> {
+        let conn = self.conn.lock()
+            .map_err(|a| a.to_string())?;
+        let mut cursor = conn
+            .prepare("SELECT full_name, telegram_id FROM users WHERE telegram_id = ? LIMIT 1")
+            .map_err(|a| a.to_string())?
+            .into_cursor()
+            .bind(&[Value::String(id.to_string())])
+            .map_err(|a| a.to_string())?;
+
+        if let Some(Ok(row)) = cursor.next() {
+            let user = Self::build_from_cursor(&row)?;
+
+            return Ok(Some(user));
+        }
+
+        Ok(None)
+    }
+
+
+    pub fn query_all_names(&self) -> Result<Vec<String>, String> {
+        let conn = self.conn.lock()
+            .map_err(|a| a.to_string())?;
+        let mut cursor = conn
+            .prepare("SELECT full_name FROM users")
+            .map_err(|a| a.to_string())?
+            .into_cursor()
+            .bind(&[])
+            .map_err(|a| a.to_string())?;
+
+        let mut users = Vec::new();
+        while let Some(Ok(row)) = cursor.next() {
+            users.push(row.try_get(0).map_err(|a| a.to_string())?);
+        }
+
+        Ok(users)
+    }
+
+
+    pub fn insert_telegram_data(&self, fullname: String, telegram_id: u64) -> Result<(), String>{
+        let conn = self.conn.lock()
+            .map_err(|a| a.to_string())?;
+
+        let mut statement = conn.prepare("UPDATE users SET telegram_id = ? WHERE full_name = ? AND telegram_id IS NULL")
+            .map_err(|a| a.to_string())?
+            .bind(1, telegram_id.to_string().as_str()).map_err(|a| a.to_string())?
+            .bind(2, fullname.to_string().as_str()).map_err(|a| a.to_string())?;
+
+
+        loop {
+            match statement.next() {
+                Ok(v) => {
+                    if matches!(v, State::Done) {
+                        break
+                    }
+                }
+                Err(a) => {
+                    return Err(a.to_string());
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,284 @@
+mod db;
+
+#[macro_use]
+extern crate log;
+
+
+use teloxide::dptree;
+
+use std::sync::Arc;
+use log::LevelFilter;
+
+use teloxide::{Bot, RequestError};
+use teloxide::dispatching::dialogue::InMemStorage;
+use teloxide::prelude::*;
+use teloxide::types::ChatKind;
+use teloxide::types::MessageKind;
+
+use teloxide::utils::command::BotCommands;
+use db::DBConn;
+use std::io::Write;
+use chrono::Local;
+use env_logger::Builder;
+
+
+const GROUP_ID: i64 = -1001509012802;
+const OWNER_ID: u64 = 429171352;
+const INVITE_LINK: &str = "https://t.me/+j-2EHIs0HqVhZjRi";
+
+#[tokio::main]
+async fn main() {
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                     "{} [{}] ({}:{}) - {}",
+                     Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                     record.level(),
+                     record.file().unwrap_or(""),
+                     record.line().unwrap_or(0),
+                     record.args(),
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+    loop {
+        if let Err(e) = run().await {
+            eprintln!("{}", e);
+        }
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
+
+async fn run() -> Result<(), String> {
+    info!("Starting...");
+    let bot = Bot::from_env().auto_send();
+
+    let dbb = Arc::new(DBConn::open()?);
+
+    Dispatcher::builder(
+        bot,
+        Update::filter_message()
+            .branch(
+                dptree::filter(|msg: Message| {
+                    if let MessageKind::Common(msg) = msg.kind {
+                        msg.from.map(|from| from.id == UserId(OWNER_ID)).unwrap_or(false)
+                    } else {
+                        false
+                    }
+                }).filter_command::<Command>().endpoint(answer)
+            )
+            .branch(
+                dptree::filter(|msg: Message| {
+                    matches!(msg.chat.kind, ChatKind::Private(_))
+                }).branch(
+                    Update::filter_message()
+                        .enter_dialogue::<Message, InMemStorage<DialogState>, DialogState>()
+                        .branch(dptree::case![DialogState::Start].endpoint(start_dialog))
+                        .branch(dptree::case![DialogState::WaitingForName].endpoint(receive_name))
+                )
+            )
+            .branch(
+                dptree::filter(|msg: Message|
+                    msg.chat.id.0 == GROUP_ID
+                )
+                    .endpoint(|m: Message, b: AutoSend<Bot>, db: Arc<DBConn>| async move {
+                        if let Err(e) = check_group_message(m, b, db.clone()).await {
+                            error!("{}", e);
+                        }
+
+                        respond(())
+                    })
+            ),
+    )
+        .dependencies(dptree::deps![dbb.clone(), InMemStorage::<DialogState>::new()])
+        .default_handler(|_| async {
+            ()
+        })
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch().await;
+
+    Ok(())
+}
+
+async fn check_group_message(m: Message, b: AutoSend<Bot>, db: Arc<DBConn>) -> Result<(), String> {
+    if let MessageKind::NewChatMembers(members) = m.kind {
+        for member in members.new_chat_members {
+            check_member(&member.id, b.clone(), db.clone()).await?
+        }
+    }
+    Ok(())
+}
+
+async fn check_member(member: &UserId, b: AutoSend<Bot>, arc: Arc<DBConn>) -> Result<(), String> {
+    let exists = arc.find_by_telegram_id(member.0)?.is_some();
+
+    if !exists {
+        b.kick_chat_member(ChatId(GROUP_ID), member.clone()).await.map_err(|a| a.to_string())?;
+        b.unban_chat_member(ChatId(GROUP_ID), member.clone()).await.map_err(|a| a.to_string())?;
+    }
+
+
+    Ok(())
+}
+
+
+#[derive(BotCommands, Clone)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "display this text.")]
+    Start,
+}
+
+async fn answer(msg: Message, bot: AutoSend<Bot>, command: Command) -> Result<(), RequestError> {
+    if !matches!(msg.chat.kind, ChatKind::Private(_)) {
+        return Ok(());
+    }
+    info!("{:?}", msg);
+    match command {
+        Command::Help | Command::Start => {
+            bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+        }
+    }
+
+    return Ok(());
+}
+
+#[derive(Clone)]
+enum DialogState {
+    Start,
+    WaitingForName,
+}
+
+impl Default for DialogState {
+    fn default() -> Self {
+        return DialogState::Start;
+    }
+}
+
+async fn start_dialog(
+    bot: AutoSend<Bot>,
+    msg: Message,
+    dialogue: Dialogue<DialogState, InMemStorage<DialogState>>,
+    db: Arc<DBConn>,
+) -> Result<(), RequestError> {
+    let registered = if let MessageKind::Common(content) = msg.kind {
+        match content.from.map(|from| db.find_by_telegram_id(from.id.0)).unwrap_or(Ok(None)) {
+            Ok(v) => { v }
+            Err(e) => {
+                error!("{}", e);
+                bot.send_message(msg.chat.id, "Техническая ошибка.").await?;
+                return Ok(());
+            }
+        }
+    } else {
+        None
+    };
+
+    if let Some(registered) = registered {
+        bot.send_message(msg.chat.id, format!("{}, кажется ты уже зарегистрирован. На всякий случай вот тебе ссылка еще раз.\n\
+        {}\n\
+        Если произошла какая-то ошибка пиши @JustAG0d", registered.full_name, INVITE_LINK)).await?;
+        return Ok(());
+    }
+
+    bot.send_message(msg.chat.id,
+                     "Привет, и добро пожаловать на КТ!\n\
+                        Прежде чем ты наконец познакомишься со своими однокурсниками мне нужно добавить тебя в чат к ним.\n\
+                        Для этого мне надо удостовериться, что ты есть в приказе на зачисление\n\
+                        Пожалуйста пришли свое полное ФИО как оно написано на https://abit.itmo.ru/\n",
+    ).send().await?;
+
+    if let Err(e) = dialogue.update(DialogState::WaitingForName).await {
+        error!("{:?}", e);
+        bot.send_message(msg.chat.id, "Техническая ошибка.").await?;
+        return Ok(());
+    }
+
+    Ok(())
+}
+
+
+async fn receive_name(
+    bot: AutoSend<Bot>,
+    msg: Message,
+    dialogue: Dialogue<DialogState, InMemStorage<DialogState>>,
+    db: Arc<DBConn>,
+) -> Result<(), RequestError> {
+    let format = "Пожалуйста отправь свое ФИО одним сообщением. Пример: Иванов Иван Иванович";
+    let text = if let Some(text) = msg.text() {
+        text.trim().to_string()
+    } else {
+        bot.send_message(msg.chat.id, format).await?;
+        return Ok(());
+    };
+
+    let user = match db.find_by_full_name(&text) {
+        Ok(v) => {
+            if v.is_some() {
+                bot.send_message(msg.chat.id, "Нашел! Секундочку...").await?;
+
+                let user = v.as_ref().unwrap();
+                let user_id = user.telegram_id.as_ref();
+
+                if user_id.is_some() {
+                    bot.send_message(msg.chat.id, format!("{}, кажется ты уже зарегистрирован. На всякий случай вот тебе ссылка еще раз.\n\
+                    {}\n\
+                    Если произошла какая-то ошибка пиши @JustAG0d", INVITE_LINK, user.full_name)).await?;
+
+                    dialogue.update(DialogState::Start).await.unwrap();
+
+                    return Ok(());
+                }
+
+                v
+            } else {
+                match db.query_all_names() {
+                    Ok(mut v) => {
+                        v.sort_by_key(|a| edit_distance::edit_distance(&a, &text));
+                        let top: Vec<String> = v.into_iter().take(5).collect();
+                        if top.len() >= 1 {
+                            bot.send_message(msg.chat.id, format!("Кажется такой фамилии нет в приказе. Но я нашел похожие:\n{}", top.join("\n"))).await?;
+                        }
+                    }
+                    Err(e) => { error!("{}", e); }
+                }
+
+                None
+            }
+        }
+        Err(e) => {
+            error!("{}", e);
+            bot.send_message(msg.chat.id, "Техническая ошибка.").await?;
+            return Ok(());
+        }
+    };
+
+    if user.is_none() {
+        bot.send_message(msg.chat.id, "Попробуй скопировать ФИО с https://abit.itmo.ru!").await?;
+        dialogue.update(DialogState::WaitingForName).await.unwrap();
+        return Ok(());
+    }
+
+    if let MessageKind::Common(content) = &msg.kind {
+        let id = content.from.as_ref().map(|a| a.id);
+        if id.is_none() {
+            bot.send_message(msg.chat.id, "Почему то не могу прочитать отправителя сообщения.").await?;
+            warn!("Cannot read sender id: {:?}", msg);
+            return Ok(());
+        }
+        let id = id.unwrap();
+        if let Err(e) = db.insert_telegram_data(text, id.0) {
+            error!("{}", e);
+            bot.send_message(msg.chat.id, "Техническая ошибка.").await?;
+            return Ok(());
+        }
+
+        bot.send_message(msg.chat.id, format!("Ура! Ты успешно прошел проверку! Теперь заходи скорей по ссылке: {}", INVITE_LINK)).await?;
+    }
+
+    return Ok(());
+}
+
